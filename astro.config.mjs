f8f4d2cd0b38846ee649a10dee2cb8c0d3cd0c8c
Arnaud Gouel <gouel.arnaud@gmail.com>
2025-07-21 16:55:03 +0200

    Set base path for portfolio deployment in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arnaudgouel.github.io',
+  base: '/portfolio-astro',
 
   integrations: [tailwind(), react(), sitemap()]
 });
